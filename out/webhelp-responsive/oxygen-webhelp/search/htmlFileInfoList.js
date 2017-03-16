5a2f4951910079c4b4090c961584a4cceb2581bd
corrinlin <clund5@vt.edu>
2017-03-16 10:36:41 -0400

    Added files

---
+++ out/webhelp-responsive/oxygen-webhelp/search/htmlFileInfoList.js
@@ -0,0 +1,7 @@
+fil = new Array();
+fil["0"]= "concepts/c-intro.html@@@Welcome to Spring Break 2k17@@@Finally, a week off from class and school. To celebrate, let&apos;s go to Chicago...";
+fil["1"]= "tasks/t-flight.html@@@How we&apos;re getting to Chicago@@@Put on your seatbelts because this is going to be a wild ride...";
+fil["2"]= "tasks/t-paying.html@@@How to pay for this trip@@@Good things in life cost money. This is one of those good things...";
+fil["3"]= "tasks/t-sleep.html@@@Sleeping Arrangements@@@You will need to sleep for three nights in Chicago. This is how it will go...";
+fil["4"]= "tasks/t-thingstodo.html@@@How to have fun in Chicago@@@If you&apos;re not a raging alcoholic, this guide is for you...";
+fil["5"]= "troubleshooting/ts-missedflight.html@@@Missed Flight@@@What to do if you&apos;ve missed your first flight...";
---
+++ out/webhelp-responsive/oxygen-webhelp/search/htmlFileList.js
@@ -0,0 +1,9 @@
+//List of files which are indexed.
+fl = new Array();
+fl["0"]= "concepts/c-intro.html";
+fl["1"]= "tasks/t-flight.html";
+fl["2"]= "tasks/t-paying.html";
+fl["3"]= "tasks/t-sleep.html";
+fl["4"]= "tasks/t-thingstodo.html";
+fl["5"]= "troubleshooting/ts-missedflight.html";
+var doStem = false;searchLoaded = true;
